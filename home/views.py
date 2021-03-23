d8933dd8c63a7c4383da4d280c498e8ccd16b6ef
fatimatirras <fatmaturrash@gmail.com>
2021-03-23 23:53:16 +0300

    deneme

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    text="merhabaaa django"
+    text="merhabaaaaaa django"
 
     context = {'text': text}
     return render(request, 'index.html', context)
