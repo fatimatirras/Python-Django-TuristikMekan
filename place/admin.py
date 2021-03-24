3fe7120024442b7e9327ad04370e79ad68885a1a
fatimatirras <fatmaturrash@gmail.com>
2021-03-24 17:46:06 +0300

    category ve place ilişkisi

--- place/admin.py
+++ place/admin.py
@@ -2,10 +2,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from place.models import Category
+from place.models import Category, Place
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'location', 'city', 'country', 'status']
+    list_filter = ['status', 'category']
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
--- place/models.py
+++ place/models.py
@@ -20,3 +20,25 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Place(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır')
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)     #category tablosuyla ilişkisi
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    location = models.CharField(max_length=225)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
