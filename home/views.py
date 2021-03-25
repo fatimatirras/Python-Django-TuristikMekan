2f41ecdf6d07236a2b29b8ba7c1096ef28b972f2
fatimatirras <fatmaturrash@gmail.com>
2021-03-25 22:53:11 +0300

    reference,hakkımızda ve iletişim sayfası

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.core.checks import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormMessage, ContactFormu
 
 
 def index(request):
@@ -10,9 +11,26 @@ def index(request):
     context = {'setting': setting, 'page': 'home'}
     return render(request, 'index.html', context)
 
+
+
 def contact(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormu()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.save()
+            messages.success(request, "Mesajınız gönderilmiştir. Teşekkürler ")
+            return HttpResponseRedirect ('/contact')
+
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page': 'contact'}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'contact.html', context)
 
 def aboutus(request):
