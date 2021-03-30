adb0b8128ad94a428ad8a110ef4dca9700742f9f
fatimatirras <fatmaturrash@gmail.com>
2021-03-30 03:52:45 +0300

    Anasayfa Uye Paneli

--- project3/urls.py
+++ project3/urls.py
@@ -23,13 +23,15 @@ from home import views
 urlpatterns = [
 
     path('', include('home.urls')),
+    path('home/', include('home.urls')),
+    path('place/', include('place.urls')),
+    path('user/', include('user.urls')),
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
     path('contact/', views.contact, name='contact'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('references/', views.references, name='references'),
-    path('home/', include('home.urls')),
-    path('place/', include('place.urls')),
-    path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_places, name='category_places'),
     path('place/<int:id>/<slug:slug>/', views.place_detail, name='place_detail'),
     path('search/', views.place_search, name='place_search'),
@@ -37,6 +39,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout_view'),
     path('login/', views.login_view, name='login_view'),
     path('signup/', views.signup_view, name='signup_view'),
+
 
 ]
 
